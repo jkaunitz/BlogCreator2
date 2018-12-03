216a4eefa352a120f5f68bdd0914d5f7a260cb35
jkaunitz <justin.kaunitz@gmail.com>
2018-12-03 12:07:44 -0800

    build inputs on signinforms as well as fornfields

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -2,12 +2,31 @@ import React, { Component } from 'react';
 
 import { reduxForm, Field } from 'redux-form';
 
+import { FormInput } from '../formFields';
+
 class SignInForm extends Component {
   render() {
       const { className } = this.props;
         return (
         <form className={`${className} sign-in-form`}>
-            sign in
+            <Field className='sign-in-form__name'
+            type='name'
+            title='Name'
+            placeholder='Enter Name'
+            name='name'
+            component={FormInput}/>
+            <Field className='sign-in-form__email'
+            type='email'
+            title='Email'
+            placeholder='Enter Email'
+            name='email'
+            component={FormInput}/>
+            <Field className='sign-in-form__password'
+            type='password'
+            title='Password'
+            placeholder='Enter Password'
+            name='password'
+            component={FormInput}/>
         </form>
     );
   }
